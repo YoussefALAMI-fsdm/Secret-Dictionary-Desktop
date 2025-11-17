6b5a405049477668dc05994ce3abc2e855eb73ed
ALAMI_Youssef <youssef.alami8@usmba.ac.ma>
2025-11-17 22:12:18 +0100

    Ajout du depandance de PostgreSQL en pom.xml

--- src/main/java/com/secret/dictionary/util/DataBase.java
+++ src/main/java/com/secret/dictionary/util/DataBase.java
@@ -1,4 +1,22 @@
 package com.secret.dictionary.util;
 
+// On va utiliser le patterns Singleton qui permet de garantir l'existance d'une seul instance de connexion
+// partagé partout => Cela garantie une seule configuration, une seule connexion, un seul objet partout dans le projet.
+
 public class DataBase { // class util qui permet de creer la DB et gerer la connexion
+
+    private static DataBase instance ;
+
+   private DataBase() {
+
+   }
+
+   public static DataBase getInstance() {
+
+       if ( instance == null )
+           instance = new DataBase() ; // Creation unique si aucune instance n'existe !
+
+       return instance ;
+   }
+
 }
