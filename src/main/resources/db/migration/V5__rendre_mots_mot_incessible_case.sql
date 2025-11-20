a95588026839749b224652de1a5e5d7bcfe0bb48
ALAMI_Youssef <youssef.alami8@usmba.ac.ma>
2025-11-20 20:28:29 +0100

    fix: renommer V6 en V5 pour corriger la numérotation 2

--- src/main/resources/db/migration/V5__rendre_mots_mot_incessible_case.sql
+++ src/main/resources/db/migration/V5__rendre_mots_mot_incessible_case.sql
@@ -1,4 +1,4 @@
--- Activer l'extension citext (si non existante) ( test )
+-- Activer l'extension citext (si non existante) ( test valide )
 CREATE EXTENSION IF NOT EXISTS citext;
 
 -- Changer le type de la colonne mot pour qu'elle soit insensible à la casse
