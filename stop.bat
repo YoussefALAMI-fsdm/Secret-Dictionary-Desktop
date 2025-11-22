9b18c3017f8b0b86c1f088aaa7f256fbae56244d
ALAMI_Youssef <youssef.alami8@usmba.ac.ma>
2025-11-22 15:59:47 +0100

    Modification legere dans la documentation

--- stop.bat
+++
@@ -1,8 +0,0 @@
-# ============================================
-# stop.bat (Windows)
-# ============================================
-@echo off
-echo Arret de PostgreSQL...
-docker-compose down
-echo PostgreSQL arrete avec succes !
-pause
