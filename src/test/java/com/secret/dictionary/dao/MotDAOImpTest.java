ea16e1f3eb76e7ec96c2c1b66854480491b7915c
ALAMI_Youssef <youssef.alami8@usmba.ac.ma>
2025-12-03 19:50:44 +0100

    Ajout des logger en initDB du MotDAOTest

--- src/test/java/com/secret/dictionary/dao/MotDAOImpTest.java
+++ src/test/java/com/secret/dictionary/dao/MotDAOImpTest.java
@@ -9,6 +9,7 @@
 package com.secret.dictionary.dao;
 
 import com.secret.dictionary.util.DataBase;
+import com.secret.dictionary.utils.SimpleLogger;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.*;
 
@@ -29,8 +30,24 @@ public class MotDAOImpTest {
     private Connection connexionH2; // Ne peut pas etre final car il est rénistailiser avant chaque test ( @BeforeEach )
     private MotDAOImp dao ;
 
+    private static final String LOG_FILE_PATH = "logs/LogMotDAOTest.log";
+    private static SimpleLogger logger ;
+
+    @BeforeAll // static car pas besion etre appartient a chaque instance de test séparer (@Test)
+    public static void initLogger() {
+        logger = new SimpleLogger(LOG_FILE_PATH, true); // true = reset fichier au début
+        logger.log("\n============= Début de la session de tests =============\n");
+    }
+
+    @AfterAll
+    public static void closeLogger() {
+        logger.log("\n============= Fin de la session de tests =============\n");
+    }
+
     @BeforeEach
     public void initDB () throws Exception { // on a Junit de gerer les Exeption : si lancé => Test echoue
+
+        logger.log("Initialisation de la base H2 pour le test.");
 
         connexionH2 = DriverManager.getConnection (
                 "jdbc:h2:mem:testdb;MODE=PostgreSQL;DATABASE_TO_UPPER=FALSE;DB_CLOSE_DELAY=-1;", // mem:testdb => Creer la DB en mémoire
@@ -44,16 +61,24 @@ public class MotDAOImpTest {
                 .locations("classpath:db/migration")
                 .load();
 
+        logger.log("Migration Flyway exécutée.");
         flyway.migrate();
 
         DataBase dbMock = mock(DataBase.class); // Puiseque notre DAO prend une BD en arguement alors on le mock
         when(dbMock.getConnection()).thenReturn(connexionH2);  // Quand mon DAO va utiliser .getConnection on va jamais l'appelé
         dao = new MotDAOImp(dbMock); // mais on va plutot passé directement le connexionH2 => DB creer réelement mais avec H2
+
+        logger.log("DAO initialisé avec la connexion H2.");
     }
 
     @AfterEach
     public void fermerConnexion () throws Exception {
         if ( connexionH2 != null && !connexionH2.isClosed() )
             connexionH2.close();
+
+        logger.log("Connexion H2 fermée après le test.");
     }
+
+
+
 }
